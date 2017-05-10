4c43a5949fc244b575846b0067ef32e5ba51849f
SeunghanJin <insightjin@gmail.com>
2017-05-10 19:04:15 +0900

    project upload

---
+++ prc/calculator/calculator/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by 진승한 on 2017. 5. 10..
+//  Copyright © 2017년 jsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("여기서 시작")
+        
+        var b = "name"
+    
+        
+        print("브레이크 포인트 연습")
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ prc/calculator/calculator/my swift.swift
@@ -0,0 +1,9 @@
+//
+//  my swift.swift
+//  calculator
+//
+//  Created by 진승한 on 2017. 5. 10..
+//  Copyright © 2017년 jsh. All rights reserved.
+//
+
+import Foundation
